8bad080fbc0b3ddc97dd5d661b98a7609a740982
mr-andersen523 <chasea523@gmail.com>
2020-03-09 17:32:57 -0700

    Register/install service worker to index.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,19 @@
+// TODO: add service worker
+
+// Register/install service worker (from google developer)
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 let transactions = [];
 let myChart;
 
