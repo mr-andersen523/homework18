951ca56f823f1812ebbc83eb7fe20be209c11450
mr-andersen523 <chasea523@gmail.com>
2020-03-09 17:25:30 -0700

    added cache/return request code (google developer)

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,3 +17,21 @@ event.waitUntil(
     })
 );
 });
+
+
+
+//Cache and return requests (from google developer)
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+    caches.match(event.request)
+        .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+        return response;
+        }
+        return fetch(event.request);
+        }
+    )
+    );
+});
